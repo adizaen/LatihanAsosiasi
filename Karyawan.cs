9f3a1ddb1645417546d841b826bab67373166d57
Adi <adi.mustaqim@students.amikom.ac.id>
2019-07-02 14:52:58 +0700

    Latihan Asosiasi

---
+++ Karyawan.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanAsosiasi
+{
+    public class Karyawan
+    {
+        public string Nama { get; set; }
+        public bool Status { get; set; }
+
+        public bool Login(Kartu kartu)
+        {
+            //hubungan Associaton
+            return kartu.IsValid(this);
+        }
+    }
+    public class Kartu
+    {
+        public bool IsValid(Karyawan karyawan)
+        {
+            return karyawan.Status == true;
+        }
+    }
+}
